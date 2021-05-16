c7da18e26fd75b5731b7900b491b18f30f0de523
Ahmad <ahmads1704@gmail.com>
2021-05-16 22:13:21 +0700

    update design

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,7 +4,7 @@ function Article({ post: { id, title, article } }) {
   return (
     <div className="overflow-hidden shadow-lg rounded-lg h-90 w-full md:w-80 cursor-pointer">
       <a href={`#${id}`} className="w-full block h-full">
-        <div className="bg-white dark:bg-gray-800 w-full p-4">
+        <div className="bg-white dark:bg-gray-800 h-full w-full p-4">
           <p className="text-indigo-500 text-md font-medium">Article</p>
           <p className="text-gray-800 dark:text-white text-xl font-medium mb-2">
             {title}
